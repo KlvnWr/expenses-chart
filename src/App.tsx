7d3318f56f1c9abb768e4214a6f88532d87d3bfe
KlvnWr <kelvin.wartanabe@gmail.com>
2024-05-17 09:42:00 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   
     <div className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Your Name Here</a>.
+      Coded by <a href="https://github.com/KlvnWr">Kelvin</a>.
     </div>
     </>
   )
