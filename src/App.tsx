46336b3a1ef60144d47dd3de66d9d5633cee6186
KlvnWr <kelvin.wartanabe@gmail.com>
2024-05-28 23:47:06 -0300

    background color set

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div id="balance">
-        <div className="">
-          <p>My balance</p>
-          <h1 className="font-bold">$921.48</h1>
+      <div id="balance" className="">
+        <div className="bg-SoftRed rounded-2xl">
+          <p className="text-white">My balance</p>
+          <h1 className="font-bold mt-2 text-2xl text-white">$921.48</h1>
+          <div id="circulo"></div>
         </div>
       </div>
       <div id="spending">
         <div>
-          <h1 className="font-bold text-Soft red">Spending - Last 7 days</h1>
+          <h1 className="font-bold text-SoftRed">Spending - Last 7 days</h1>
           mon tue wed thu fri sat sun
           <div>Total this month $478.33</div>
           <div>+2.4% from last month</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ export default {
   ],
   theme: {
     colors: {
-      'Soft red': 'hsl(10, 79%, 65%)',
+      'SoftRed': 'hsl(10, 79%, 65%)',
       'Cyan': 'hsl(186, 34%, 60%)',
-      'Dark brown': 'hsl(25, 47%, 15%)',
-      'Medium brown': 'hsl(28, 10%, 53%)',
+      'DarkBrown': 'hsl(25, 47%, 15%)',
+      'MediumBrown': 'hsl(28, 10%, 53%)',
       'Cream': 'hsl(27, 66%, 92%)',
-      'Very pale orange': 'hsl(33, 100%, 98%)'
+      'VeryPaleOrange': 'hsl(33, 100%, 98%)'
     },
     extend: {},
   },
