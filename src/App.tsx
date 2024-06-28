d0cb4e02b66c29c6652d97c29b70ca80553393f4
KlvnWr <kelvin.wartanabe@gmail.com>
2024-06-28 13:32:52 -0300

    im back

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,38 @@ function App() {
   return (
     <>
       <div id="balance" className="">
-        <div className="bg-SoftRed rounded-2xl p-5 m-5 min-w-96">
-          <p className="text-Cream text-left">My balance</p>
-          <h1 className="font-bold mt-2 text-2xl text-Cream text-left">
-            $921.48
-          </h1>
-          <div id="circulo"></div>
+        <div className=" flex bg-SoftRed rounded-2xl p-5 m-5 min-w-96">
+          <div className="flex-1">
+            <p className="text-Cream text-left">My balance</p>
+            <h1 className="font-bold mt-2 text-2xl text-Cream text-left">
+              $921.48
+            </h1>
+          </div>
+          <div id="circulo" className="float-right flex-2 pt-2">
+            <svg
+              width="72"
+              height="48"
+              viewBox="0 0 72 48"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <circle fill="#382314" cx="48" cy="24" r="24" />
+                <circle stroke="#FFF" stroke-width="2" cx="24" cy="24" r="23" />
+              </g>
+            </svg>
+          </div>
         </div>
       </div>
       <div id="spending">
         <div>
-          <h1 className="font-bold text-SoftRed">Spending - Last 7 days</h1>
-          mon tue wed thu fri sat sun
-          <div>Total this month $478.33</div>
-          <div>+2.4% from last month</div>
+          <h1 className="font-bold text-DarkBrown">Spending - Last 7 days</h1>
+          <p className="text-MediumBrown">mon tue wed thu fri sat sun</p>
+          <div className="text-MediumBrown">
+            Total this month <span className="text-DarkBrown">$478.33</span>
+          </div>
+          <div className="text-MediumBrown">
+            <span className="text-DarkBrown">+2.4%</span> from last month
+          </div>
         </div>
 
         <div className="attribution">
